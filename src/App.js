55c8d06d1b2f65a6c2016ce37aa49e197eb8edc3
jungil0617 <jungil0617@naver.com>
2024-11-21 19:10:02 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // CSS를 따로 분리
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* 헤더 */}
+            <header className="header">
+                <img src={`${process.env.PUBLIC_URL}/basic_logo.gif`} alt="단국대학교병원 로고" />
+            </header>
+
+            {/* 메인 콘텐츠 */}
+            <main className="main">
+                <div className="button-container">
+                    <button
+                        className="button"
+                        onClick={() => alert('첫 번째 버튼 클릭!')}
+                    >
+                        진료 예약
+                    </button>
+                    <button
+                        className="button"
+                        onClick={() => alert('두 번째 버튼 클릭!')}
+                    >
+                        진료 일정 안내
+                    </button>
+                    <button
+                        className="button"
+                        onClick={() => alert('세 번째 버튼 클릭!')}
+                    >
+                        병원 시설 안내
+                    </button>
+                    <button
+                        className="button"
+                        onClick={() => alert('네 번째 버튼 클릭!')}
+                    >
+                        예약 조회
+                    </button>
+                </div>
+            </main>
+
+            {/* 푸터 */}
+            <footer className="footer">무인 안내 데스크</footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>이것은 내가 만든 컴포넌트입니다.</p>
+        </div>
+    );
+}
+
+export default MyComponent;
